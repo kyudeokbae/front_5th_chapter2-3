a3871ba448d2e011e488ea0b81df98cc1903451c
배규덕 <mandarinduk@gmail.com>
2025-04-28 22:46:04 +0900

    Header, Footer 컴포넌트 위치 변경

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "../widgets/ui/Header.tsx"
-import Footer from "../widgets/ui/Footer.tsx"
 import PostsManagerPage from "../pages/PostsManagerPage.tsx"
+import { Footer } from "../widgets/footer/footer.ui.tsx"
+import { Header } from "../widgets/header/header.ui.tsx"
 
 const App = () => {
   return (
